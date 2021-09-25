f2253c31bdc5f561ee255fbce97a63ee50013f8a
ftnext <takuyafjp+develop@gmail.com>
2021-09-25 22:55:24 +0900

    [chore] Enable to install from GitHub

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="xdays_journey",
+    version="0.1.0",
+    packages=find_packages(include=["xdays_journey"]),
+    python_requires=">=3.7",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: Japanese",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    url="https://github.com/pyconjp/x-days-journey",
+    author="PyCon JP 2021 staff",
+    author_email="pyconjp@pycon.jp",
+    license="MIT license",
+    zip_sage=False,
+)
