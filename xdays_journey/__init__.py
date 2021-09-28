921f8cbe91e3acd882c9a25cf61f8f85eee293e0
ftnext <takuyafjp+develop@gmail.com>
2021-09-28 20:17:52 +0900

    [feat] Fix hardcode; enable to pass date to あと何日

--- xdays_journey/__init__.py
+++ xdays_journey/__init__.py
@@ -1,5 +1,7 @@
 from datetime import date
 
+PYCONJP_2021_DAY1 = date(2021, 10, 15)
 
-def あと何日():
-    return date(2021, 10, 15) - date.today()
+
+def あと何日(the_day=PYCONJP_2021_DAY1):
+    return the_day - date.today()
