7ac986e1df6a3a7bb6e58c38c65e9c08558b4635
ftnext <takuyafjp+develop@gmail.com>
2021-09-29 23:33:49 +0900

    [docs] Show help message

--- xdays_journey/__main__.py
+++ xdays_journey/__main__.py
@@ -1,3 +1,16 @@
+from argparse import ArgumentParser, RawDescriptionHelpFormatter
+
 from xdays_journey import あと何日
+
+description = """あなたのXデーまであと何日かを表示
+
+デフォルトではPyCon JP 2021 (2021/10/15) まであと何日かを表示します。"""
+
+parser = ArgumentParser(
+    prog="python -m xdays_journey",
+    description=description,
+    formatter_class=RawDescriptionHelpFormatter,
+)
+parser.parse_args()
 
 print(あと何日())
