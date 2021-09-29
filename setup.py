ee93a3cf8098619904d0d5f7a95be99c2c5aa097
ftnext <takuyafjp+develop@gmail.com>
2021-09-29 23:34:34 +0900

    [chore] Bump up with docs

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="xdays_journey",
-    version="0.3.0",
+    version="0.3.1",
     packages=find_packages(include=["xdays_journey"]),
     python_requires=">=3.7",
     classifiers=[
